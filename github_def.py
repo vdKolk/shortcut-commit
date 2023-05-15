c8b6f5436cc4639e40e7ab482ad4e00d037c2429
RMEngels <r.m.engels@scienta.nl>
2023-05-15 10:35:08 +0200

    Use system instead of popen to avoid runtime conflicts

--- github_def.py
+++ github_def.py
@@ -5,7 +5,7 @@ from shortcut_def import Shortcut
 class GitHub:
     @staticmethod
     def __add_all_to_commit() -> None:
-        os.popen(f'git -C {os.getcwd()} add .')
+        os.system(f'git -C {os.getcwd()} add .')
 
     @staticmethod
     def create_commit() -> None:
@@ -14,5 +14,5 @@ class GitHub:
 
         GitHub.__add_all_to_commit()
 
-        os.popen(f'git -C {os.getcwd()} commit -m "Fixes [sc-{story_number}] {title}" --no-verify')
-        os.popen(f'git -C {os.getcwd()} push -u origin feature-develop/sc-{story_number}')
+        os.system(f'git -C {os.getcwd()} commit -m "Fixes [sc-{story_number}] {title}" --no-verify')
+        os.system(f'git -C {os.getcwd()} push -u origin feature-develop/sc-{story_number}')
